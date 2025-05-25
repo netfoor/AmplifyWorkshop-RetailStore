e5fc490aebdad529415338b2dbe2b84296fb3fec
Fortino Romero <2311080254@alumno.utpuebla.edu.mx>
2025-05-25 13:15:28 -0600

    feat: Create iu-forms with linked GraphQL API actions usign npx ampx generate forms npx ampx generate graphql-client-code commands provided by amplify to basic components or the capability to build with exported graphql declaration in src/graphql

---
+++ src/ui-components/graphql/subscriptions.ts
@@ -0,0 +1,169 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateCategory = /* GraphQL */ `
+  subscription OnCreateCategory(
+    $filter: ModelSubscriptionCategoryFilterInput
+    $owner: String
+  ) {
+    onCreateCategory(filter: $filter, owner: $owner) {
+      createdAt
+      description
+      id
+      image
+      name
+      owner
+      products {
+        nextToken
+        __typename
+      }
+      styles
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const onCreateProduct = /* GraphQL */ `
+  subscription OnCreateProduct(
+    $filter: ModelSubscriptionProductFilterInput
+    $owner: String
+  ) {
+    onCreateProduct(filter: $filter, owner: $owner) {
+      category {
+        createdAt
+        description
+        id
+        image
+        name
+        owner
+        styles
+        updatedAt
+        __typename
+      }
+      categoryProductId
+      createdAt
+      current_stock
+      description
+      id
+      image
+      name
+      owner
+      price
+      rating
+      style
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const onDeleteCategory = /* GraphQL */ `
+  subscription OnDeleteCategory(
+    $filter: ModelSubscriptionCategoryFilterInput
+    $owner: String
+  ) {
+    onDeleteCategory(filter: $filter, owner: $owner) {
+      createdAt
+      description
+      id
+      image
+      name
+      owner
+      products {
+        nextToken
+        __typename
+      }
+      styles
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const onDeleteProduct = /* GraphQL */ `
+  subscription OnDeleteProduct(
+    $filter: ModelSubscriptionProductFilterInput
+    $owner: String
+  ) {
+    onDeleteProduct(filter: $filter, owner: $owner) {
+      category {
+        createdAt
+        description
+        id
+        image
+        name
+        owner
+        styles
+        updatedAt
+        __typename
+      }
+      categoryProductId
+      createdAt
+      current_stock
+      description
+      id
+      image
+      name
+      owner
+      price
+      rating
+      style
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const onUpdateCategory = /* GraphQL */ `
+  subscription OnUpdateCategory(
+    $filter: ModelSubscriptionCategoryFilterInput
+    $owner: String
+  ) {
+    onUpdateCategory(filter: $filter, owner: $owner) {
+      createdAt
+      description
+      id
+      image
+      name
+      owner
+      products {
+        nextToken
+        __typename
+      }
+      styles
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const onUpdateProduct = /* GraphQL */ `
+  subscription OnUpdateProduct(
+    $filter: ModelSubscriptionProductFilterInput
+    $owner: String
+  ) {
+    onUpdateProduct(filter: $filter, owner: $owner) {
+      category {
+        createdAt
+        description
+        id
+        image
+        name
+        owner
+        styles
+        updatedAt
+        __typename
+      }
+      categoryProductId
+      createdAt
+      current_stock
+      description
+      id
+      image
+      name
+      owner
+      price
+      rating
+      style
+      updatedAt
+      __typename
+    }
+  }
+`;
