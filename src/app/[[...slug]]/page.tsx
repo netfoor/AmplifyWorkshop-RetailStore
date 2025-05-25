ff9a607cbeafc71134fed91458d4438c6fee5f87
Fortino Romero <2311080254@alumno.utpuebla.edu.mx>
2025-05-25 14:03:28 -0600

    feat: Add Authentication in a modal iu-component on the frontend. First we defined the context on App.tsx, and then we import the component in header.jsx

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React from 'react';
 import dynamic from 'next/dynamic';
-import '../lib/amplifyClient'; 
+import '../../lib/amplifyClient'; 
 
 const App = dynamic(() => import('../../App'), { ssr: false })
 
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,10 +6,12 @@ import ProgressBar from '@cloudscape-design/components/progress-bar';
 import SpaceBetween from '@cloudscape-design/components/space-between';
 import TopNavigation from '@cloudscape-design/components/top-navigation';
 
+import { Authenticator, useAuthenticator } from '@aws-amplify/ui-react';
+
 import { LoadSampleData } from '../utils/data-loader/loader';
 
 export default function Header(props) {
-  const { user, signOut } = { user: undefined, signOut: () => {} };
+  const { user, signOut } = useAuthenticator((context) => [context.user]);
   const [visible, setVisible] = useState(false);
   const [loadDataVisible, setLoadDataVisible] = useState(false);
   const [categoryProgress, setCategoryProgress] = useState(0);
@@ -123,6 +125,7 @@ export default function Header(props) {
       header="Please sign in or create an account"
       modalRoot={document.getElementById('top-nav')}
     >
+      <Authenticator />
 
     </Modal>
   </div>
